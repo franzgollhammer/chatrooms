2f48f17e80a89e7560a72eb251cee670e4498203
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-02-24 18:43:35 +0100

    :rocket: Added documentation for helper functions

--- server/src/userHelper.js
+++ server/src/userHelper.js
@@ -1,5 +1,23 @@
+/**
+ * Helper functions for users:
+ * User = {
+ *  username: '',
+ *  chatroom: ''
+ * }
+ */
+
 const users = [];
 
+//TODO: Error handling, type checks
+
+/**
+ * Adds a user to the users list
+ * @param id - this is the client socket id 
+ * @param username - client username
+ * @param chatroom - chatroom name
+ * @returns {error} - if username already exists else,
+ * @returns {null} - and adds the user to the userlist
+ */
 const addUser = ({ id, username, chatroom }) => {
     username = username.trim().toLowerCase();
     chatroom = chatroom.trim().toLowerCase();
@@ -8,6 +26,7 @@ const addUser = ({ id, username, chatroom }) => {
         return user.chatroom === chatroom && user.username === username
     });
 
+    // check if username already exists
     existingUser !== null ? { error: 'Username is already taken!' } : null;
 
     const user = { id, username, chatroom };
@@ -15,13 +34,27 @@ const addUser = ({ id, username, chatroom }) => {
     users.push(user);
 }
 
+/**
+ * Removes a user from the userlist.
+ * @param id - this is the client socket id
+ */
 const removeUser = id => {
     const idx = users.findIndex((user) => user.id === id);
     idx !== -1 ? users.splice(idx, 1)[0] : null;
 }
 
+/**
+ * Finds a user in the userlist with the client socket id
+ * @param id - this is the client socket id
+ * @returns - A single user
+ */
 const getUser = id => users.find(user => user.id === id);
 
+/**
+ * Finds all users in a given chatroom.
+ * @param chatroom - Name of a chatroom
+ * @returns {List} - List of users in the chatroom
+ */
 const getUsersInRoom = chatroom => users.filter(user => user.chatroom === chatroom);
 
 module.exports = {
